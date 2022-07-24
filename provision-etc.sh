4e9ef73be099e43199038326992ac43cb9c7489a
megutamago <31330976+megutamago@users.noreply.github.com>
2022-07-25 01:33:12 +0900

    Update provision-etc.sh

--- provision-etc.sh
+++ provision-etc.sh
@@ -6,14 +6,14 @@ sudo apt install -y zsh
 echo $SHELL
 chsh -s $(which zsh)
 #再ログイン or zsh?
-#rm .bashrc .bash_history .bash_profile
+rm .bashrc .bash_history .bash_profile
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 # 対話式コマンドで設定
-prompt -s powerlevel10k
+# prompt -s powerlevel10k
 
 
 #peco
