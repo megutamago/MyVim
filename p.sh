bb209aced0d56ab246590adf71a35428cce091e3
megutamago <31330976+megutamago@users.noreply.github.com>
2022-08-09 23:42:54 +0900

    Update p.sh

--- p.sh
+++ p.sh
@@ -4,19 +4,19 @@ cp ~/MyVim/.colorrc ~/
 
 ### dein.vim setting
 mkdir ~/.config
-sudo apt install -y python3-dev python3-pip software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo add-apt-repository ppa:jonathonf/vim -y
-sudo apt update -y
-sudo apt install neovim -y
+apt install -y python3-dev python3-pip software-properties-common
+add-apt-repository ppa:neovim-ppa/unstable -y
+add-apt-repository ppa:jonathonf/vim -y
+apt update -y
+apt install neovim -y
 
 mkdir ~/docs && cd ~/docs
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 
-sudo pip3 install -U pip
-sudo pip3 install msgpack
-sudo pip3 install pynvim
+pip3 install -U pip
+pip3 install msgpack
+pip3 install pynvim
 
 cp -r ~/MyVim/myvim ~/.config/nvim
 mv ~/.config/nvim/install.vim ~/.config/nvim/init.vim
