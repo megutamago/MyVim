4be562c6413ac04714061d868c20834c4dc1bad1
megutamago <31330976+megutamago@users.noreply.github.com>
2022-08-09 21:52:09 +0900

    Update p2.sh

--- p2.sh
+++ p2.sh
@@ -1,4 +1,10 @@
-#/bin/bash -eux
+#/bin/bash
+
+set -eu
 
 mv ~/.config/nvim/init.vim ~/.config/nvim/install.vim
 mv ~/.config/nvim/tmp.vim ~/.config/nvim/init.vim
+
+sudo apt install -y zsh
+
+chsh -s $(which zsh)
