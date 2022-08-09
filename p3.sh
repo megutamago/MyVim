fc0666448005799b0e352c4b6f3ac9a27d37b727
megutamago <31330976+megutamago@users.noreply.github.com>
2022-08-09 21:51:43 +0900

    Update p3.sh

--- p3.sh
+++ p3.sh
@@ -2,9 +2,6 @@
 
 set -eu
 
-sudo apt install -y zsh
-
-chsh -s $(which zsh)
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
